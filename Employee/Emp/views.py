6c03962233d072b48992ebbd62715295ea64f98d
rajsekhar <rajshekarreddyr19@gmail.com>
2021-04-01 16:33:43 -1200

    day9

---
+++ Employee/Emp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+	return render(request,'html/home.html')
+
+def about(request):
+	return render(request,'html/about.html')
+
+def cont(request):
+	return render(request,'html/cont.html')
+
+def login(request):
+	return render(request,'html/login.html')
